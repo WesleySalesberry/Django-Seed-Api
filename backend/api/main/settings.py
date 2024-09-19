73c0d9b34ac96c5578d6fa91cb33e9b2e2248791
WesleySalesberry <61122432+WesleySalesberry@users.noreply.github.com>
2024-09-19 08:40:22 -0400

    Models (#2)
    * Inital DB setup
    
    * Updated to add auth user and add to installed apps
    
    * Added user model and wrote custom auth model
    
    ---------
    
    Co-authored-by: Wes <wesleySalesberry@outlook.com>

--- backend/api/main/settings.py
+++ backend/api/main/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'dbmodels',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'dbmodels.User'
